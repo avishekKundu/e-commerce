fa7024d0d0b3ba7a3d557f24247ed8b062c99319
Avishek Kundu <119733078+avishekKundu@users.noreply.github.com>
2023-09-25 20:59:27 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
+import Product from './Body';
+import Footer from './Footer';
+import Header from './Header';
+import SideBar from './SideBar';
+
 
 function App() {
   return (
     <>
+      <Header />
+      <SideBar />
+      <Product />
+      <Footer />
     </>
   );
 }
